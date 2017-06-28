928a3274bf2aadb26d156c53a6123b5fcd338302
ross128 <ross128@geekmail.de>
2017-06-28 12:20:17 +0200

    added Tranchot maps

--- js/map.js
+++ js/map.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 	//layers
 	var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {});
-	
+	var tranchot = L.tileLayer.wms("http://www.wms.nrw.de/geobasis/wms_nw_tranchot", {
+		layers: 'nw_tranchot',
+		format: 'image/png',
+	});
+	var tranchot_info = L.tileLayer.wms("http://www.wms.nrw.de/geobasis/wms_nw_tranchot", {
+		layers: 'nw_tranchot_info',
+		format: 'image/png',
+	});
+
 	var map = L.map('map', {
 		center: [51, 6.3],
 		zoom: 10,
@@ -10,7 +18,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	});
 
 	L.control.layers({
-		'mapnik': osm,
+		'Mapnik': osm,
+		'Tranchot': tranchot,
+		'Tranchot Info': tranchot_info,
 	}).addTo(map);
 	
 });
